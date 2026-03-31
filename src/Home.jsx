7f1a0f14c64009e0728b57c02f9c06c8c1c436a6
ThakaliAnkeet <thakaliankeet@gmail.com>
2026-03-31 16:33:51 +0545

    First Commit

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+function Home(){
+    
+    return(
+        <div className="home">
+            <div className="banner">
+                <p>Having Trouble Finding Courts for Your Next Game?</p>
+                <p>Let us Help You</p>
+            </div>
+            <div className="courts-near-you">
+                <h3>Courts Near You</h3>
+            </div>
+        </div>
+    );
+}
+export default Home;
